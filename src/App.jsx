20f486879d8e64cf9293b0896bf0b2340e6fb164
Kyle J. Harms <kyle.harms@cornell.edu>
2023-09-12 09:01:47 -0400

    conditional render for edit mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ParagraphEdit from './components/ParagraphEdit';
 export default function App({ children }) {
   return (
     <div className="App">
-      <ParagraphEdit>
+      <ParagraphEdit editMode>
         Your components will often need to display different things depending on different conditions. In React, you can conditionally render JSX using JavaScript syntax like if statements, &&, and ? : operators. Source: https://react.dev/learn/conditional-rendering
       </ParagraphEdit>
     </div>
--- src/components/ParagraphEdit.jsx
+++ src/components/ParagraphEdit.jsx
@@ -1,9 +1,25 @@
 import './ParagraphEdit.css';
 
-export default function ParagraphEdit({ children }) {
+export default function ParagraphEdit({ children, editMode = false }) {
+
+  let componentContent;
+  if (editMode) {
+    componentContent = (
+      <>
+        <textarea defaultValue={children} />
+      </>
+    )
+  } else {
+    componentContent = (
+      <>
+        <p>{children}</p>
+      </>
+    )
+  }
+
   return (
     <>
-      {children}
+      {componentContent}
     </>
   );
 }
