d78f3c81e13a3270577e1cf18ddd0de1783905df
Stephan Volynets <svv6@cornell.edu>
2024-10-16 18:03:37 -0400

    Trying Parallax alteration for particles. Adding structure for the rest of the pages.

--- src/components/pageHeaderContent/index.jsx
+++ src/components/pageHeaderContent/index.jsx
@@ -1,19 +1,18 @@
 import "./styles.scss";
-
-
+import ParticlesComponent from "../ParticlesComponent";
 
 const PageHeaderContent = (props) => {
-
-    const {headerText, icon} = props;
+    const { headerText, icon } = props;
 
     return (
         <div className="wrapper">
-            <h2>{headerText}</h2>
-            <span>{icon}</span>
-
-
+            <ParticlesComponent id="tsparticles-header" />
+            <div className="content">
+                <h2>{headerText}</h2>
+                <span>{icon}</span>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default PageHeaderContent;
